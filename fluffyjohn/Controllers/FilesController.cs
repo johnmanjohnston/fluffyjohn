e0eba08982714ddf7470a6832ddd3caa460b8415
johnmanjohnston <c.ethanjohn@gmail.com>
2023-02-09 21:05:43 +0400

    Add FilesController and Index page for files viewing

---
+++ fluffyjohn/Controllers/FilesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fluffyjohn.Controllers
+{
+    public class FilesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
