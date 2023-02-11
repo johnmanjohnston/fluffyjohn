9e394ee7dc1a5dfe917341d8d726acc8a9d53663
johnmanjohnston <c.ethanjohn@gmail.com>
2023-02-11 21:42:28 +0400

    Working file uploads with POST req

--- fluffyjohn/Controllers/FilesController.cs
+++ fluffyjohn/Controllers/FilesController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.IO;
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Text;
 
 namespace fluffyjohn.Controllers
 {
-    public class FilesController : Controller
-    {
-        [Route("/Files/{**dirpath}")]
+    public class FilesController : Controller { 
+        [Route("/ViewFiles/{**dirpath}")]
         public IActionResult Index()
         {
             if (!User.Identity!.IsAuthenticated)
@@ -13,6 +17,38 @@ namespace fluffyjohn.Controllers
             }
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload()
+        {
+            var filePath = Directory.GetCurrentDirectory() + "/UserFileStorer/";
+
+
+            foreach (var formFile in Request.Form.Files)
+            {
+                if (formFile.Length > 0) 
+                {
+                    using (var inputStream = new FileStream(filePath + formFile.FileName, FileMode.Create))
+                    {
+                        // read file to stream
+                        await formFile.CopyToAsync(inputStream);
+                        // stream to byte array
+                        byte[] array = new byte[inputStream.Length];
+                        inputStream.Seek(0, SeekOrigin.Begin);
+                        inputStream.Read(array, 0, array.Length);
+                        // get file name
+                        string fName = formFile.FileName;
+                    }
+                }
+            }
+
+                    return new ContentResult
+            {
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = $"<html><body>Welcome{filePath}</body></html>"
+            };
         }
     }
 }
