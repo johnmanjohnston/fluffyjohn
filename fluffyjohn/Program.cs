9f19d4ee4907967366e979aeadad72fd47c2572f
johnmanjohnston <c.ethanjohn@gmail.com>
2023-02-14 22:05:10 +0400

    Progress on dir selector

--- fluffyjohn/Program.cs
+++ fluffyjohn/Program.cs
@@ -17,9 +17,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseExceptionHandler("/Error");
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) { 
+app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
---
+++ fluffyjohn/wwwroot/js/dirSelect.js
@@ -0,0 +1,13 @@
+﻿const STORAGE_ROUTE = `${location.protocol}//${location.host}`;
+const PARENT_DIRS = window.location.href.replace(STORAGE_ROUTE + "/ViewFiles/", "").split("/");
+
+const dirSelectorEl = document.getElementById("dir-selector");
+var pathProgression = "";
+
+for (var dirIndex = 0; dirIndex < PARENT_DIRS.length; dirIndex++) {
+    const dirName = PARENT_DIRS[dirIndex];
+    pathProgression += dirName;
+
+    dirSelectorEl.innerHTML += "/" + pathProgression + "/ <br />"
+}
+
