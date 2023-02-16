340c14ea842b5a7277339347d7b01b240a6426d7
johnmanjohnston <c.ethanjohn@gmail.com>
2023-02-16 18:08:21 +0400

    Moved path formatting to seperate class and added Utility class

---
+++ fluffyjohn/PathFormatter.cs
@@ -0,0 +1,28 @@
+﻿using fluffyjohn.Utility;
+
+namespace fluffyjohn
+{
+    public static class PathFormatter
+    {
+        private static void StrRemove(ref string orgStr, string match) { orgStr = orgStr.Replace(match, string.Empty); }
+
+        public static string[] FormatEntries(
+            ref string[] entries, string reqPath, string rootUserDir)
+        {
+            string[] formattedEntries = entries;
+            
+            for (int i = 0; i < formattedEntries.Length; i++)
+            {
+                StrRemove(ref formattedEntries[i], rootUserDir);
+
+                formattedEntries[i] = formattedEntries[i].Replace("//", "/");
+                formattedEntries[i] = formattedEntries[i].Replace("\\", "/");
+
+                try { StrRemove(ref formattedEntries[i], reqPath); }
+                catch (ArgumentException) { }
+            }
+
+            return formattedEntries;
+        }
+    }
+}
---
+++ fluffyjohn/Utility.cs
@@ -0,0 +1,11 @@
+﻿namespace fluffyjohn.Utility
+{
+    public static class Utility
+    {
+        public static void StrRemove(ref string orgStr, string match) 
+        { 
+            if (match.Length > orgStr.Length) { throw new ArgumentException("Match cannot be bigger than orginal string"); }
+            orgStr = orgStr.Replace(match, string.Empty);
+        }
+    }
+}
