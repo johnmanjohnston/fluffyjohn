91729ab125a4d200f3132647bdad4e50f11c5016
johnmanjohnston <c.ethanjohn@gmail.com>
2023-02-16 19:51:33 +0400

    Add function to configure href for directory hyperlinks

--- fluffyjohn/Utility.cs
+++ fluffyjohn/Utility.cs
@@ -7,5 +7,17 @@
             if (match.Length > orgStr.Length) { throw new ArgumentException("Match cannot be bigger than orginal string"); }
             orgStr = orgStr.Replace(match, string.Empty);
         }
+
+        public static string ConfigureDirHref(string href)
+        {
+            int dirSplits = href.Split("/").Length;
+
+            for (int i = 0; i < dirSplits; i++)
+            {
+                href = href.Replace("//", "/");
+            }
+
+            return href;
+        }
     }
 }
