5533bf716bac86c2cfb469e7fb450303e846e25a
johnmanjohnston <c.ethanjohn@gmail.com>
2023-02-17 21:31:34 +0400

    UI changes, periodic runner changes

--- fluffyjohn/wwwroot/css/periodicrun.cpp
+++ fluffyjohn/wwwroot/css/periodicrun.cpp
@@ -5,13 +5,35 @@
 #include <thread>
 
 int iteration = 0;
+int iterationCap = -1; // Run infinitely 
+bool countLogs = true;
+
+void Sleep() 
+{
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+}
+
+void RecursiveRun() 
+{
+    if (iteration >= iterationCap) 
+    {
+        exit(0);
+    }
+
+    iteration++;
+
+    if (countLogs) 
+    {
+        std::cout << "Running iteration " << iteration << "...\n";
+    }
+
+    system("sass storage.scss storage.css");
+    Sleep();
+
+    RecursiveRun();
+}
 
 int main() 
 {
-    iteration++;
-    std::cout << "Running iteration " << iteration << "...\n";
-    system("sass storage.scss storage.css");
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-
-    main();
+    RecursiveRun();
 }
