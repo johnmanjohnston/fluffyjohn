9da266d1a3e875ede2d705ee4e351f70a17ed09a
johnmanjohnston <c.ethanjohn@gmail.com>
2023-02-19 19:15:40 +0400

    Dir selector changes, added numeric id to every file and dir

--- fluffyjohn/wwwroot/js/dirSelect.js
+++ fluffyjohn/wwwroot/js/dirSelect.js
@@ -1,9 +1,8 @@
 var DIR_SELECT_EL = document.getElementById("dir-selector");
-var absoluteHomeURL = "".concat(location.protocol, "//").concat(location.host).toLowerCase();
-var dirs = window.location.href.toLowerCase().replace(absoluteHomeURL + "/viewfiles/", "").split("/");
+var absoluteHomeURL = "".concat(location.protocol, "//").concat(location.host);
+var dirs = window.location.href.replace(absoluteHomeURL + "/viewfiles/", "").split("/");
 var path_progression = "";
 for (var dirindex = 0; dirindex < dirs.length; dirindex++) {
-    dirs[dirindex] = dirs[dirindex].toLowerCase();
     path_progression += "".concat(dirs[dirindex], "/");
     // DIR_SELECT_EL.innerHTML += path_progression + "<br>"
     if (dirs[dirindex].toString() !== "")
--- fluffyjohn/wwwroot/js/dirselect.ts
+++ fluffyjohn/wwwroot/js/dirselect.ts
@@ -1,11 +1,10 @@
 ﻿const DIR_SELECT_EL: HTMLElement = document.getElementById("dir-selector");
 
-const absoluteHomeURL: string = `${location.protocol}//${location.host}`.toLowerCase();
-const dirs: string[] = window.location.href.toLowerCase().replace(absoluteHomeURL + "/viewfiles/", "").split("/");
+const absoluteHomeURL: string = `${location.protocol}//${location.host}`;
+const dirs: string[] = window.location.href.replace(absoluteHomeURL + "/viewfiles/", "").split("/");
 var path_progression: string = "";
 
 for (var dirindex = 0; dirindex < dirs.length; dirindex++) {
-    dirs[dirindex] = dirs[dirindex].toLowerCase();
     path_progression += `${dirs[dirindex]}/`
     // DIR_SELECT_EL.innerHTML += path_progression + "<br>"
 
