b59994c22c61431961ac0f66cb05c0445dceba1b
johnmanjohnston <c.ethanjohn@gmail.com>
2023-04-02 20:16:50 +0400

    Fixed bug where going to /viewfiles (without the ending "/") would recognize the domain as a dir

--- fluffyjohn/wwwroot/js/dirSelect.js
+++ fluffyjohn/wwwroot/js/dirSelect.js
@@ -1,10 +1,9 @@
 var DIR_SELECT_EL = document.getElementById("dir-selector");
 var absoluteHomeURL = "".concat(location.protocol, "//").concat(location.host);
-var dirs = window.location.href.replace(absoluteHomeURL + "/viewfiles/", "").split("/");
+var dirs = window.location.href.replace(absoluteHomeURL + "/viewfiles", "").split("/");
 var path_progression = "";
 for (var dirindex = 0; dirindex < dirs.length; dirindex++) {
     path_progression += "".concat(dirs[dirindex], "/");
-    // DIR_SELECT_EL.innerHTML += path_progression + "<br>"
     if (dirs[dirindex].toString() !== "")
         DIR_SELECT_EL.innerHTML += "<a class=\"dir-select\" href=\"/viewfiles/".concat(path_progression, "\">").concat(dirs[dirindex], "/</a>");
 }
