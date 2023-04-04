41a80867f28205c6020f4fee616dd619b9dbbc1c
johnmanjohnston <c.ethanjohn@gmail.com>
2023-04-04 18:41:13 +0400

    Removed commented, old confirmDelete() implementation

--- fluffyjohn/wwwroot/js/delete.js
+++ fluffyjohn/wwwroot/js/delete.js
@@ -1,13 +1,3 @@
-/*function confirmDelete(contentName: string, contentPath: string, isFile: boolean) {
-    if (confirm(`Are you sure you want to delete ${contentName}? This is not reversable.`) === true) {
-        if (isFile) {
-            document.location = `/delfile/${contentPath}`;
-        } else {
-            document.location = `/deldir/${contentPath}`;
-        }
-    }
-}
-*/
 function confirmDelete(contentName, contentPath, isFile) {
     if (confirm("Are you sure you want to delete ".concat(contentName, "? This is not reversable.")) === true) {
         var data = {
--- fluffyjohn/wwwroot/js/delete.ts
+++ fluffyjohn/wwwroot/js/delete.ts
@@ -1,15 +1,4 @@
-﻿/*function confirmDelete(contentName: string, contentPath: string, isFile: boolean) {
-    if (confirm(`Are you sure you want to delete ${contentName}? This is not reversable.`) === true) {
-        if (isFile) {
-            document.location = `/delfile/${contentPath}`;
-        } else {
-            document.location = `/deldir/${contentPath}`;
-        }
-    }
-}
-*/
-
-function confirmDelete(contentName: string, contentPath: string, isFile: boolean) {
+﻿function confirmDelete(contentName: string, contentPath: string, isFile: boolean) {
     if (confirm(`Are you sure you want to delete ${contentName}? This is not reversable.`) === true) {
 
         let data = {
